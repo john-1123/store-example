4991e284459d6674514effc0744ce6ae97bc26ec
john-1123 <johnwowhn@gmail.com>
2021-08-06 18:15:09 +0800

    Initial commit

---
+++ static/js/index.js
@@ -0,0 +1,26 @@
+/*slide 自動換圖片*/
+var slideIndex = 0;
+showSlides(slideIndex);
+setInterval(showSlides, 3000);
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    if (slideIndex >= slides.length) {slideIndex = 0;}
+    console.log(slideIndex);
+
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex].style.display = "block";
+    dots[slideIndex].className += " active";
+    slideIndex++;
+}
